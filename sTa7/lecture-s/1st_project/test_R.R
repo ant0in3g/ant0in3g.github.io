52803a9222f07ef663bcd5fcebfaef9cb8090cda
Antoine Géré <a.gere@istom.fr>
2025-01-23 15:59:41 +0100

    -

--- sTa7/lecture-s/1st_project/test_R.R
+++ sTa7/lecture-s/1st_project/test_R.R
@@ -143,4 +143,28 @@ mat_add_column
 plot(1, 3)
 plot(c(1, 8), c(3, 10)) 
 
-plot(1:10, main="My Graph", xlab="The x-axis", ylab="The y axis",col="green",cex=0.5) 
+plot(1:10, main="My Graph", xlab="The x-axis", ylab="The y axis",col="red",cex=0.5,pch=11) 
+
+# day one, the age and speed of 12 cars:
+x1 <- c(5,7,8,7,2,2,9,4,11,12,9,6)
+y1 <- c(99,86,87,88,111,103,87,94,78,77,85,86)
+
+# day two, the age and speed of 15 cars:
+x2 <- c(2,2,8,1,15,8,12,9,7,3,11,4,7,14,12)
+y2 <- c(100,105,84,105,90,99,90,95,94,100,79,112,91,80,85)
+
+plot(x1, y1, main="Observation of Cars", xlab="Car age", ylab="Car speed", col="red", cex=2)
+points(x2, y2, col="blue", cex=2) 
+
+
+x <- c(10,20,30,40)
+mylabel <- c("Apples", "Bananas", "Cherries", "Dates")
+colors <- c("blue", "yellow", "green", "black")
+pie(x,, label = mylabel, main = "Fruits",col = colors,init.angle = 90) 
+legend("bottomright", mylabel, fill = colors) 
+
+
+x <- c("A", "B", "C", "D")
+y <- c(2, 4, 6, 8)
+barplot(y, names.arg = x, col = "red",density = 10,width = c(1,2,3,4))
+barplot(y, names.arg = x, horiz = TRUE) 
